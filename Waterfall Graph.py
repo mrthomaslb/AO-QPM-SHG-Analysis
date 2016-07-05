a30d7274c6f55f29cb882a44d6eefc8db59acff8
Thomas Lehman-Borer <mrthomaslb@users.noreply.github.com>
2016-07-05 15:39:44 -0400

    Waterfall Graph works now
    Specifically for the lineouts of the data taken on 6/30. Adjustments needed to look at other lineouts.

--- Waterfall Graph.py
+++ Waterfall Graph.py
@@ -1,29 +1,55 @@
  # -*- coding: utf-8 -*-
 
-from mpl_toolkits.mplot3d import Axes3D
+from mpl_toolkits.mplot3d import Axes3D #needed for projection='3d'
 from matplotlib.collections import PolyCollection
 from matplotlib.colors import colorConverter
+from tkinter import filedialog
 import matplotlib.pyplot as plt
 import numpy as np
 
+def lineout_to_array():
+    dat = filedialog.askopenfile()
+    datload = np.loadtxt(dat)
+    datcrop = datload[:,1:4]
+    return datcrop
+
+pick = int(input("""Select one of the following:
+    1: 402 nm (scans 11-21)
+    2: 414 nm (scans 2-10)
+    3: 411 nm (scans 2-10)
+
+>>>"""))
+while pick!=1 and pick!=2 and pick!=3:
+    pick = input("""Please just type 1, 2, or 3.
+
+>>>""")
+
+if pick==2 or pick==3:
+    zs = [420/97, 371/97, 320/97, 269/97, 220/98, 171/96, 121/97, 1, 80/97]
+    zcounter = 1
+else:
+    zs = [413/96, 311/96, 120/97, 100/97, 75/98, 50/97, 25/97, 219/98, 170/98, 271/98, 371/98]
+    zcounter = 10
 
 fig = plt.figure()
 ax = fig.gca(projection='3d')
 
-
 def cc(arg):
     return colorConverter.to_rgba(arg, alpha=0.6)
 
-xs = np.arange(-1.38, 1.38, 0.1)
 verts = []
-zs = [4.329896907, 3.824742268, 3.298969072, 2.773195876, 2.244897959, 1.78125, 1.24742268, 1, 0.824742268]
-for z in zs:
-    ys = np.random.rand(len(xs))
-    ys[0], ys[-1] = 0, 0
-    verts.append(list(zip(xs, ys)))
+phaseshift = 0
 
-poly = PolyCollection(verts, facecolors=[cc('r'), cc('g'), cc('b'),
-                                         cc('y')])
+for z in zs:
+    xs = list(np.arange(-1.38+phaseshift, 1.38+phaseshift, 0.0138))
+    phaseshift-=0.0138
+    zcounter += 1
+    print('Select scan %d lineout' % zcounter)
+    ys = lineout_to_array()[:,1:2]
+    verts.append([(-1.38,0)]+list(zip(xs, ys))+[(1.382
+    ,0)])
+
+poly = PolyCollection(verts, facecolors=[cc('r'), cc('g'), cc('b'), cc('y')])
 poly.set_alpha(0.7)
 ax.add_collection3d(poly, zs=zs, zdir='y')
 
@@ -32,6 +58,6 @@ ax.set_xlim3d(-1.38, 1.38)
 ax.set_ylabel('Power Ratio')
 ax.set_ylim3d(0, 4.5)
 ax.set_zlabel('% Change in Intensity')
-ax.set_zlim3d(0, 1)
+ax.set_zlim3d(-.05, .05)
 
 plt.show()
